92fc0290f94f0ca62e9226433d7df27556a1581c
ewelinaszoda <ewelinaszoda@gmail.com>
2020-06-30 11:27:28 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
 // create your App component here
-// import React from 'react';
+import React from 'react';
 
-// class App extends React.Component {
-
-//   state = {
-//     peopleInSpace: [],
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
-//       </div>
-//     )
-//   }
-
-//   function getPeople() {
-//   return fetch('http://api.open-notify.org/astros.json')
-//     .then(response => response.json())
-//     .then(data => data.people)
-// }
-//  componentDidMount() {
-//   getPeople()
-//     .then(people => this.setState({ peopleInSpace: people }))
-// }
-
-// }
-
-// export default App
-
-
-import React, { Component } from 'react'
-
-const url = `http://api.open-notify.org/astros.json`
-
-class App extends Component {
+class App extends React.Component {
 
   state = {
-    peopleInSpace: []
+    peopleInSpace: [],
   }
 
   render() {
     return (
       <div>
-        {/* //DIFFERENT ???*/}
-        {this.state.peopleInSpace.map((person, idx) => <h1 key={idx}>{person.name}</h1>)}
         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
       </div>
     )
   }
 
-  componentDidMount() {
-    fetch(url)
+  getPeople = () => {
+    return fetch('http://api.open-notify.org/astros.json')
       .then(response => response.json())
-      .then(data => this.setState({ peopleInSpace: data.people }))
+      .then(data => data.people)
   }
+  componentDidMount() {
+    this.getPeople()
+      .then(people => this.setState({ peopleInSpace: people }))
+  }
+
 }
 
 export default App
+
+
+// import React, { Component } from 'react'
+
+// const url = `http://api.open-notify.org/astros.json`
+
+// class App extends Component {
+
+//   state = {
+//     peopleInSpace: []
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {/* //DIFFERENT ???*/}
+//         {/* {this.state.peopleInSpace.map((person, idx) => <h1 key={idx}>{person.name}</h1>)} */}
+//         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+//       </div>
+//     )
+//   }
+
+//   componentDidMount() {
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(data => this.setState({ peopleInSpace: data.people }))
+//   }
+// }
+
+// export default App
